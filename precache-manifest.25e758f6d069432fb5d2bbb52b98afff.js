e2f9e74d2eed50d28fa8916a502245fc8cfce274
mjaracz <michal19996@gmail.com>
2019-03-13 17:49:42 +0100

    Updates

---
+++ precache-manifest.25e758f6d069432fb5d2bbb52b98afff.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "383f5a65ae447b871b12",
+    "url": "/snake/static/js/runtime~main.383f5a65.js"
+  },
+  {
+    "revision": "1d7eb4d697c5c0cdd259",
+    "url": "/snake/static/js/main.1d7eb4d6.chunk.js"
+  },
+  {
+    "revision": "8dfd21e55608c0f4fc62",
+    "url": "/snake/static/js/2.8dfd21e5.chunk.js"
+  },
+  {
+    "revision": "1d7eb4d697c5c0cdd259",
+    "url": "/snake/static/css/main.f95fae33.chunk.css"
+  },
+  {
+    "revision": "628bf7be40a82ae5b3ad57afbcd648a4",
+    "url": "/snake/index.html"
+  }
+];
